f287b5b7ce4c72cc7537d4fdef7d6e5b5b653943
Musaraf Hossain <146651402+musarafhossain@users.noreply.github.com>
2024-08-20 20:09:06 +0530

    change link

--- js/gAuth.js
+++ js/gAuth.js
@@ -1,6 +1,6 @@
 const CLIENT_ID = '936389827719-p29mt159u8qa0hkricvrecvtrcp9ofpl.apps.googleusercontent.com';
-const REDIRECT_URI = 'http://127.0.0.1:5500/';//For Development
-//const REDIRECT_URI = 'https://musarafhossain.github.io/FaceFilterPro/';//For Production
+//const REDIRECT_URI = 'http://127.0.0.1:5500/';//For Development
+const REDIRECT_URI = 'https://musarafhossain.github.io/FaceFilterPro/';//For Production
 const SCOPE = 'https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive.readonly https://www.googleapis.com/auth/userinfo.profile';
 
 const API_KEY = 'AIzaSyCnwIHjGfkPzfjKfBvE_2pGHWOiyYqN6yM';
